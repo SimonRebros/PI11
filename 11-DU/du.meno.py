d8846057b7a1b8a8a52c9bf79ab62ea59a3703f2
simon <simonrebros07@gmail.com>
2023-11-14 17:43:21 +0100

    domaca uloha

---
+++ 11-DU/du.meno.py
@@ -0,0 +1,114 @@
+import tkinter
+canvas = tkinter.Canvas()
+canvas.pack()
+
+x = 5
+y = 5
+d = 10
+r = "red"
+b = "black"
+yellow = "yellow"
+
+canvas.create_rectangle(x + d, y , x +  2 * d, y + d,fill=b)
+canvas.create_rectangle(x , y + d , x + d, y + 2 * d,fill=b)
+canvas.create_rectangle(x , y + 2 * d , x +  d, y + 3 * d,fill=b)
+canvas.create_rectangle(x + 2 * d, y + 3 * d , x + 3 * d, y + 4 * d,fill=b)
+canvas.create_rectangle(x + 2 * d, y + 6 * d , x + 3 * d, y + 7 * d,fill=b)
+canvas.create_rectangle(x + 4 * d, y + d , x + 5 * d, y,fill=b)
+canvas.create_rectangle(x + d, y + 6 * d , x + 2 * d, y + 7 * d,fill=b)
+canvas.create_rectangle(x + d, y + 3 * d , x + 2 * d, y + 4 * d,fill=b)
+canvas.create_rectangle(x + 2 * d, y  , x + 3 * d, y +  d,fill=b)
+canvas.create_rectangle(x , y + 6 * d , x + d, y + 7 * d,fill=b)
+canvas.create_rectangle(x + 3 * d, y + 3 * d , x + 4 * d, y + 4 * d,fill=b)
+canvas.create_rectangle(x + 3 * d, y + 6 * d , x + 4 * d, y + 7 * d,fill=b)
+canvas.create_rectangle(x + 3 * d, y    , x + 4 * d, y +  d,fill=b)
+canvas.create_rectangle(x + 4 * d, y + 4 * d , x + 5 * d, y + 5 * d,fill=b)
+canvas.create_rectangle(x + 4 * d, y + 5 * d , x + 5 * d, y + 6 * d,fill=b)
+
+x = 6 * d
+y = 5
+d = 10
+r = "red"
+b = "black"
+yellow = "yellow"
+
+canvas.create_rectangle(x, y , x + d, y + d,fill=b)
+canvas.create_rectangle(x + d, y  , x + 2 * d, y + d,fill=b)
+canvas.create_rectangle(x + 2 * d, y  , x + 3 * d, y +  d,fill=b)
+canvas.create_rectangle(x +  d, y +  d , x + 2 * d, y + 2*d,fill=b)
+canvas.create_rectangle(x + d, y + 3 * d , x + 2 * d, y + 4 * d,fill=b)
+canvas.create_rectangle(x +  d, y + 2 * d , x + 2 * d, y + 3 * d,fill=b)
+canvas.create_rectangle(x +d, y + 4 * d , x + 2*d, y + 5 * d,fill=b)
+canvas.create_rectangle(x +d, y + 5 * d , x + 2*d, y + 6 * d,fill=b)
+canvas.create_rectangle(x +d, y + 6 * d , x + 2*d, y + 7 * d,fill=b)
+canvas.create_rectangle(x  , y + 6 * d , x +  d, y + 7 * d,fill=b)
+canvas.create_rectangle(x +2*d , y + 6 * d , x + 3* d, y + 7 * d,fill=b)
+
+x = 10 * d
+y = 5
+d = 10
+b = "black"
+canvas.create_rectangle(x, y , x + d, y + d,fill=b)
+canvas.create_rectangle(x + d, y +d , x + 2 * d, y + 2*d,fill=b)
+canvas.create_rectangle(x + 3*d, y +d , x + 4 * d, y + 2*d,fill=b)
+canvas.create_rectangle(x + 2 * d, y +3*d , x + 3 * d, y + 4* d,fill=b)
+canvas.create_rectangle(x , y +  d , x +  d, y + 2*d,fill=b)
+canvas.create_rectangle(x , y + 3 * d , x +  d, y + 4 * d,fill=b)
+canvas.create_rectangle(x , y + 2 * d , x +  d, y + 3 * d,fill=b)
+canvas.create_rectangle(x , y + 4 * d , x + d, y + 5 * d,fill=b)
+canvas.create_rectangle(x , y + 5 * d , x + d, y + 6 * d,fill=b)
+canvas.create_rectangle(x +4*d, y + 6 * d , x + 5*d, y + 7 * d,fill=b)
+canvas.create_rectangle(x +4*d, y + 5 * d , x + 5*d, y + 6 * d,fill=b)
+canvas.create_rectangle(x +4*d, y + 4 * d , x + 5*d, y + 5 * d,fill=b)
+canvas.create_rectangle(x +4*d, y + 3 * d , x + 5*d, y + 4 * d,fill=b)
+canvas.create_rectangle(x +4*d, y + 2 * d , x + 5*d, y + 3 * d,fill=b)
+canvas.create_rectangle(x +4*d, y +  d , x + 5*d, y + 2 * d,fill=b)
+canvas.create_rectangle(x +4*d, y   , x + 5*d, y +  d,fill=b)
+canvas.create_rectangle(x  , y + 6 * d , x +  d, y + 7 * d,fill=b)
+canvas.create_rectangle(x +2*d , y + 2 * d , x + 3* d, y + 3 * d,fill=b)
+
+x = 15 * d
+y = 5
+d = 10
+b = "black"
+canvas.create_rectangle(x+2*d, y , x + 3*d, y + d,fill=b)
+canvas.create_rectangle(x + 3*d, y  , x + 4 * d, y + d,fill=b)
+canvas.create_rectangle(x + 4 * d, y  , x + 5 * d, y +  d,fill=b)
+canvas.create_rectangle(x +d, y + 5 * d , x + 2*d, y + 6 * d,fill=b)
+canvas.create_rectangle(x +d, y + 4 * d , x + 2*d, y + 5 * d,fill=b)
+canvas.create_rectangle(x +d, y + 3 * d , x + 2*d, y + 4 * d,fill=b)
+canvas.create_rectangle(x +d, y + 2 * d , x +2* d, y + 3 * d,fill=b)
+canvas.create_rectangle(x +d, y +  d , x +2* d, y + 2 * d,fill=b)
+canvas.create_rectangle(x +5*d, y + 5 * d , x + 6*d, y + 6 * d,fill=b)
+canvas.create_rectangle(x +5*d, y + 4 * d , x + 6*d, y + 5 * d,fill=b)
+canvas.create_rectangle(x +5*d, y + 3 * d , x + 6*d, y + 4 * d,fill=b)
+canvas.create_rectangle(x +5*d, y + 2 * d , x +6* d, y + 3 * d,fill=b)
+canvas.create_rectangle(x +5*d, y +  d , x +6* d, y + 2 * d,fill=b)
+canvas.create_rectangle(x+2*d, y +6*d, x + 3*d, y + 7*d,fill=b)
+canvas.create_rectangle(x + 3*d, y +6*d , x + 4 * d, y + 7*d,fill=b)
+canvas.create_rectangle(x + 4 * d, y +6*d , x + 5 * d, y +  7*d,fill=b)
+
+x = 22 * d
+y = 5
+d = 10
+b = "black"
+canvas.create_rectangle(x +4*d, y + 6 * d , x + 5*d, y + 7 * d,fill=b)
+canvas.create_rectangle(x +4*d, y + 5 * d , x + 5*d, y + 6 * d,fill=b)
+canvas.create_rectangle(x +4*d, y + 4 * d , x + 5*d, y + 5 * d,fill=b)
+canvas.create_rectangle(x +4*d, y + 3 * d , x + 5*d, y + 4 * d,fill=b)
+canvas.create_rectangle(x +4*d, y + 2 * d , x + 5*d, y + 3 * d,fill=b)
+canvas.create_rectangle(x +4*d, y +  d , x + 5*d, y + 2 * d,fill=b)
+canvas.create_rectangle(x +4*d, y   , x + 5*d, y +  d,fill=b)
+canvas.create_rectangle(x , y + 6 * d , x +d, y + 7 * d,fill=b)
+canvas.create_rectangle(x , y + 5 * d , x + d, y + 6 * d,fill=b)
+canvas.create_rectangle(x , y + 4 * d , x + d, y + 5 * d,fill=b)
+canvas.create_rectangle(x , y + 3 * d , x + d, y + 4 * d,fill=b)
+canvas.create_rectangle(x , y + 2 * d , x + d, y + 3 * d,fill=b)
+canvas.create_rectangle(x , y +  d , x + d, y + 2 * d,fill=b)
+canvas.create_rectangle(x , y   , x + d, y +  d,fill=b)
+canvas.create_rectangle(x +d, y + 2 * d , x + 2*d, y + 3 * d,fill=b)
+canvas.create_rectangle(x +2*d, y + 3 * d , x + 3*d, y + 4 * d,fill=b)
+canvas.create_rectangle(x +3*d, y + 4 * d , x + 4*d, y + 5 * d,fill=b)
+
+
+canvas.mainloop()
